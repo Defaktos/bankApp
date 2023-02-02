51fefed24a3fbafd54449a6d7eac9ece72dd4165
Dima Blokhin <vanilaaabear@gmail.com>
2023-02-03 00:54:47 +0600

    upd

---
+++ main.py
@@ -0,0 +1,34 @@
+class BankAccount:
+    def __init__(self, name, balance=0.0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"{amount} deposited to the account.")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= amount
+            print(f"{amount} withdrawn from the account.")
+
+    def transfer(self, to_account, amount):
+        if amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.withdraw(amount)
+            to_account.deposit(amount)
+            print(f"{amount} transferred to {to_account.name}.")
+
+    def show_balance(self):
+        print(f"Name: {self.name}\nBalance: {self.balance}")
+
+if __name__ == '__main__':
+    blokhinDima = BankAccount("John Doe")
+    blokhinaAlina = BankAccount("Jane Doe")
+    blokhinDima.deposit(1000)
+    blokhinDima.transfer(blokhinaAlina, 500)
+    blokhinDima.show_balance()
+    blokhinaAlina.show_balance()
